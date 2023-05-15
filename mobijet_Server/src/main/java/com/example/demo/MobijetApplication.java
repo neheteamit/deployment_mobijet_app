52188055da6a51d35e58c1698bd63238fe703d62
Amit Nehete <130889398+neheteamit@users.noreply.github.com>
2023-05-15 15:02:05 +0530

    Update MobijetApplication.java

--- mobijet_Server/src/main/java/com/example/demo/MobijetApplication.java
+++ mobijet_Server/src/main/java/com/example/demo/MobijetApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.example.demo.*")
+@postconstruct
 public class MobijetApplication {
 
 	public static void main(String[] args) {
